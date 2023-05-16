0594cb70cba7cae22824fe858599e0fdfd75443a
Sara De Souza Costa <111135831+SaraSouzzz@users.noreply.github.com>
2023-05-16 19:44:26 -0300

    Add files via upload
    Teste do JFRAME para a TASKKILL

---
+++ Jframe/src/main/java/com/mycompany/jframe/Jframe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jframe;
+
+/**
+ *
+ * @author sara.scosta
+ */
+public class Jframe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
